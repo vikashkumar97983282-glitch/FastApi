6ac785811511ecfb853455d07bc8a13e5618d390
Vikash Kumar Sharma <vikashkumar97983282@gmail.com>
2026-02-06 14:47:16 +0530

    add path parameter in api.

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+import json
 
 app = FastAPI()
+
+
+def load_data():
+    with open("practical.json", "r") as file:
+        data = json.load(file)
+    return data
 
 @app.get("/")
 def hello():
@@ -9,3 +16,19 @@ def hello():
 @app.get("/about")
 def about():
     return {"message":"this is about page of fastapi"}
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
+
+
+@app.get("/cust/{cust_id}")
+def view_cust(cust_id: str = Path(..., description="ID of the customer to view", example="P001")):
+
+    data = load_data()
+
+    if cust_id in data:
+        return data[cust_id]
+    return {"message": "data not found"}
